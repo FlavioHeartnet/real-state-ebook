3d862427339e63317c8e91d6f51247231aaa8a0e
Flavio <flavionogueirabarros@gmail.com>
2024-12-05 15:44:12 -0300

    resolve mail sending

--- src/app/api/usecase/sendMail.ts
+++ src/app/api/usecase/sendMail.ts
@@ -1,51 +1,47 @@
-import axios from "axios";
+'use server';
+import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
 
-interface EmailData {
-    from: { email: string };
-    to: { email: string }[];
-    subject: string;
-    html: string;
-  }
-  
+const mailerSend = new MailerSend({
+    apiKey: process.env.NEXT_PUBLIC_MAILERSEND_KEY || '',
+});
 export default async function sendEmail(email: string, name: string){
-    const emailData: EmailData = {
-        from: { email: 'NobleRealState@realstate.com' },
-        to: [{ email: email }],
-        subject: 'Seu E-book Gratuito - Estratégias Infalíveis para Vender Imóveis',
-        html: `
-            <h1>Olá ${name},</h1>
+    const sentFrom = new Sender("noblerealstate@noblespace.pro", "Marcelo Pain");
+    const recipients = [
+        new Recipient(email, name)
+    ];
     
-            <p>Estou feliz em compartilhar que seu e-book "Estratégias Infalíveis para Vender Imóveis" está pronto para download!</p>
-            
-            <p>Neste guia, você encontrará técnicas poderosas e comprovadas para:</p>
-            
-            <ul>
-                <li>Destacar seus imóveis e atrair mais compradores</li>
-                <li>Estratégias de marketing online e off-line</li>
-                <li>Dicas para fechar negócios mais rapidamente</li>
-                <li>Muito mais conselhos de especialistas do ramo!</li>
-            </ul>
-            
-            <p>Clique no link abaixo para baixar seu e-book agora:</p>
-            
-            <p><a href="https://drive.google.com/file/d/11WhtwK8zQYFh1TAygs7RgHNngVRZ-Lko/view">[Link de Download]</a></p>
-            
-            <p>Aproveite este recurso valioso para impulsionar suas vendas imobiliárias! Se tiver alguma dúvida, é só entrar em contato.</p>
-            
-            <p>Boa leitura e sucesso em suas vendas!</p>
-            
-            <p>Atenciosamente,<br>
-            Marcelo Pain</p>`,
-      };
+    const emailData:string =  `
+    <h1>Olá ${name},</h1>
+
+    <p>Estou feliz em compartilhar que seu e-book "Estratégias Infalíveis para Vender Imóveis" está pronto para download!</p>
+    
+    <p>Neste guia, você encontrará técnicas poderosas e comprovadas para:</p>
+    
+    <ul>
+        <li>Destacar seus imóveis e atrair mais compradores</li>
+        <li>Estratégias de marketing online e off-line</li>
+        <li>Dicas para fechar negócios mais rapidamente</li>
+        <li>Muito mais conselhos de especialistas do ramo!</li>
+    </ul>
+    
+    <p>Clique no link abaixo para baixar seu e-book agora:</p>
+    
+    <p><a href="https://drive.google.com/file/d/11WhtwK8zQYFh1TAygs7RgHNngVRZ-Lko/view">[Link de Download]</a></p>
+    
+    <p>Aproveite este recurso valioso para impulsionar suas vendas imobiliárias! Se tiver alguma dúvida, é só entrar em contato.</p>
+    
+    <p>Boa leitura e sucesso em suas vendas!</p>
+    
+    <p>Atenciosamente,<br>
+    Marcelo Pain</p>`
+    const emailParams = new EmailParams()
+    .setFrom(sentFrom)
+    .setTo(recipients)
+    .setSubject("Seu E-book Gratuito - Estratégias Infalíveis para Vender Imóveis")
+    .setHtml(emailData)
     try {
-        const response = await axios.post('https://api.mailersend.com/v1/email', emailData, {
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest',
-            'Authorization': `Bearer ${process.env.NEXT_PUBLIC_MAILERSEND_KEY}`,
-          },
-        });
-        console.log('Email sent successfully:', response.data);
+        const response = await mailerSend.email.send(emailParams);
+        console.log('Email sent successfully:', response.statusCode);
       } catch (error) {
         console.error('Error sending email:', error);
       }
